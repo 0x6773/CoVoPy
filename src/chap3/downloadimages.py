a1384f18173c7a439a08b3664da83d3c7b99b827
Govind Sahai <gsiitbhu@gmail.com>
2015-12-20 15:33:52 +0530

    downloadimages.py Modified

--- src/chap3/downloadimages.py
+++ src/chap3/downloadimages.py
@@ -7,26 +7,40 @@ from urlparse import urlsplit
 from bs4 import BeautifulSoup
 
 def findImages(url):
-    print '[ + ] Finding images on ' + url
+    print '[ + ] Getting HTML of %s' % url
     urlContent = urllib2.urlopen(url).read()
     soup = BeautifulSoup(urlContent, 'lxml')
+    print '[ + ] Finding images on %s' % url
     imgTags = soup.findAll('img')
+    if len(imgTags) != 0:
+        print '[ + ] Total image(s) found : %d' % len(imgTags)
+    else:
+        print '[ - ] No images found at %s' % url
     for imgTag in imgTags:
         yield imgTag
 
 def downloadImage(imgTag):
     try:
-        print '[ + ] Downloading image...'
         imgSrc = imgTag['src']
-        imgContent  = urllib2.urlopen(imgSrc).read()
+        imgOpen = urllib2.urlopen(imgSrc)
+        imgInfo = imgOpen.info()
+        imgSize = int(imgInfo.getheaders('Content-Length')[0]) / 1024
         imgFileName = basename(urlsplit(imgSrc)[2])
-        print '[ + ] Saving to ' + imgFileName
+        print '[ + ] Image FileName : %s, Image Size : %d KB' % (imgFileName, imgSize)
+        print '[ + ] Ready download ([Y]/N) : ',
+        opt = raw_input()
+        if opt == 'n' or opt == 'N':
+            print '[ - ] Not Downloading!'
+            return
+        print '[ + ] Downloading image...'
+        imgContent  = urllib2.urlopen(imgSrc).read()
+        print '[ + ] Saving to %s' % imgFileName
         imgFile = open(imgFileName, 'wb')
         imgFile.write(imgContent)
         imgFile.close()
         return imgFileName
     except Exception, e:
-        print 'Exception occurred ' + str(e)
+        print 'Exception occurred : %s' % str(e)
         return ''
 
 def main():
